8f5ddd512e3ea5e876c8df75eac06d90e45e06eb
iulia.kovacs <iulia.kovacs@student.upt.ro>
2024-01-20 23:05:28 +0200

    Added first steps for Route Registration

---
+++ RouteRegistration/routeRegistrationUtils.cpp
@@ -0,0 +1,167 @@
+#include "routeRegistrationUtils.h"
+#include <ctime> 
+#include <iostream> 
+#include <fstream>
+#include <optional>
+/* for testing purpose*/
+#include<unistd.h>  
+
+
+using namespace std; 
+
+
+RouteRegistration::RouteRegistration()
+{
+    this->register_enabled = REGISTRATION_DISABLED;
+}
+
+string RouteRegistration::get_current_timestamp()
+{
+    time_t tt; 
+    struct tm* ti; 
+    time(&tt); 
+    ti = localtime(&tt);
+    string myString = asctime(ti);
+    myString = myString.substr(0, myString.find_last_not_of(" \t\n\r") + 1); 
+    return myString; 
+}
+
+bool RouteRegistration::create_route_file(string route_name)
+{   
+    this->current_route_name = route_name;
+    this->route_names.push_back(route_name);
+    ofstream Route_File(route_name + ".txt");
+    Route_File.close();
+    return true;
+}
+
+
+void RouteRegistration::end_registration()
+{   
+    register_current_move("stop");
+    this->register_enabled = REGISTRATION_DISABLED;
+}
+
+
+void RouteRegistration::register_current_move(string move_name, optional<int> arg_value)
+{
+    ofstream Route_File;
+    Route_File.open(this->current_route_name + ".txt", ios_base::app);
+
+    if (arg_value) 
+    {
+        Route_File << get_current_timestamp() + " -> " + move_name + "(" << arg_value.value() << ")" << endl;   
+    }          
+    else
+    {
+        Route_File << get_current_timestamp() + " -> " + move_name + "(" + ")" << endl;
+    }
+            
+    Route_File.close();
+}
+
+
+void RouteRegistration::set_register_enabled_true()
+{   
+    this->register_enabled = REGISTRATION_ENABLED;
+}
+
+int RouteRegistration::extract_the_seconds_between_two_commands(string command_1, string command_2)
+{
+    string minutes_1 = command_1.substr(14,2);
+    string minutes_2 = command_2.substr(14,2);
+    string seconds_1 = command_1.substr(17,2);
+    string seconds_2 = command_2.substr(17,2);
+    //cout << "Command 1: Minutes: " << minutes_1 << " and seconds: " << seconds_1 << endl;
+    //cout << "Command 2: Minutes: " << minutes_2 << " and seconds: " << seconds_2 << endl;
+    int difference_in_seconds = (stoi(minutes_2) - stoi(minutes_1)) * 60 + (stoi(seconds_2) - stoi(seconds_1));
+    //cout << "Difference in seconds between first and second command: " << difference_in_seconds << endl;
+    return difference_in_seconds;
+}
+
+void RouteRegistration::create_record_for_route()
+{
+    string line, prev_line;
+    ifstream Route_File("optimized_" + current_route_name + ".txt");
+    ofstream Record_File;
+    Record_File.open("record_" + current_route_name + ".txt", ios_base::trunc);
+
+    if (Route_File.is_open())
+    {
+        getline(Route_File,prev_line);
+
+        while (getline(Route_File,line))
+        {
+            int duration_in_seconds = extract_the_seconds_between_two_commands(prev_line, line);
+            Record_File << prev_line.substr(28) << endl;
+            Record_File << duration_in_seconds << endl;
+            prev_line = line;
+        }
+        Route_File.close();
+    }
+}
+
+void RouteRegistration::delete_duplicate_line_commands()
+{
+    string line, prev_line;
+    ifstream Route_File(current_route_name + ".txt");
+    ofstream Optimized_File;
+    Optimized_File.open("optimized_" + current_route_name + ".txt", ios_base::trunc);
+
+    if (Route_File.is_open())
+    {
+        getline(Route_File,prev_line);
+
+        Optimized_File << prev_line << endl;
+
+        while (getline(Route_File,line))
+        {   
+            string prev_cmd = prev_line.substr(28);
+            string curr_cmd = line.substr(28);
+
+            if (prev_cmd.compare(curr_cmd) != 0)
+            {
+                Optimized_File << line << endl;
+            }
+
+            prev_line = line;
+        }
+
+        Route_File.close();
+    }
+}
+
+
+/* function for test only */
+int main() 
+{ 
+    RouteRegistration routeRegistration = RouteRegistration();
+
+    routeRegistration.create_route_file("Route_No_1");
+
+    routeRegistration.set_register_enabled_true();
+
+    routeRegistration.register_current_move("forward", 10);
+
+    routeRegistration.register_current_move("forward", 10);
+
+    sleep(3);
+
+    routeRegistration.register_current_move("stop");
+
+    routeRegistration.register_current_move("backward", 10);
+
+    routeRegistration.register_current_move("set_dir_servo_angle", 10);
+
+    sleep(3);
+
+    routeRegistration.register_current_move("set_cam_pan_angle", 80);
+
+    routeRegistration.register_current_move("set_camera_tilt_angle", 80);
+
+    routeRegistration.delete_duplicate_line_commands();
+
+    routeRegistration.create_record_for_route();
+
+    return 0; 
+}
---
+++ RouteRegistration/routeRegistrationUtils.h
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <optional>
+
+
+#define REGISTRATION_DISABLED false
+#define REGISTRATION_ENABLED true
+ 
+using namespace std;
+
+class RouteRegistration
+{
+    bool register_enabled;
+    vector<string> route_names;  
+    /* vector<string> route_file_names; */  
+    /* @ToDo convention that the file name uses route_name with suffix .txt (?) */
+    string current_route_name;
+    
+public:
+
+    RouteRegistration();
+    string get_current_timestamp();
+    bool create_route_file(string route_name);
+    int extract_the_seconds_between_two_commands(string command_1, string command_2);
+    void register_current_move(string move_name, optional<int> arg_value = nullopt);
+    void delete_duplicate_line_commands(); 
+    void create_record_for_route(); 
+    void end_registration();
+    void set_register_enabled_true();
+};
