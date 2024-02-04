9c90d7263726feecf61ab45500db923adbd78213
IuliaKovacs <iulia.kovacs@student.upt.ro>
2024-02-04 21:56:04 +0200

    Added Line Follower implementation

--- LineFollower/lineFollower.cpp
+++ LineFollower/lineFollower.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 VisionVoyager* LineFollower::robotVisionVoyager = nullptr;
 vector<int> LineFollower::grayscale_data;
+int LineFollower::stop_counter = 0;
+State LineFollower::last_state = State::UNKNOWN;
 
 void LineFollower::setRobot(VisionVoyager* robot)
 {
@@ -19,21 +21,156 @@ void LineFollower::get_grayscale_data()
 vector<int> LineFollower::get_line_from_grayscale_data()
 {
     vector<int> line_status = {
-        static_cast<int>(grayscale_data[0] < 80),
-        static_cast<int>(grayscale_data[1] < 80),
-        static_cast<int>(grayscale_data[2] < 80)
+        static_cast<int>(grayscale_data[0] < LINE_THRESHOLD),
+        static_cast<int>(grayscale_data[1] < LINE_THRESHOLD),
+        static_cast<int>(grayscale_data[2] < LINE_THRESHOLD)
     };
-    
+
     return line_status;
 }
 
 
 void LineFollower::follow_line()
 {
+    State current_state = State::UNKNOWN;
+    /* center direction servo before starting */
+    robotVisionVoyager->set_dir_angle(DEFAULT_WHEEL_ANGLE);
+    robotVisionVoyager->set_speed(1);
+
     while(true)
     {
+        if (verify_is_in_air())
+        {
+            robotVisionVoyager->stop();
+            break;
+        }     
+
+        cout << stop_counter << endl;
+        if (verify_stop_condition())
+        {
+            break;
+        }
+
         get_grayscale_data();
-        cout << grayscale_data[0] << "  " << grayscale_data[1] << "  " << grayscale_data[2] << endl;
-        // get_line_from_grayscale_data();
+        // cout << "Grayscale Data: " << grayscale_data[0] << "  " << grayscale_data[1] << "  " << grayscale_data[2] << endl;
+        vector<int> grayscale_line_status = get_line_from_grayscale_data();
+        cout << "Grayscale Line Status: " << grayscale_line_status[0] << "  " << grayscale_line_status[1] << "  " << grayscale_line_status[2] << endl;
+        last_state = current_state;
+        current_state = compute_next_state(grayscale_line_status);
+        execute_move(current_state);
+        
+    }
+}
+
+
+bool LineFollower::verify_is_in_air()
+{
+    get_grayscale_data();
+    return ((grayscale_data[0] <= IN_AIR_THRESHOLD) && (grayscale_data[1] <= IN_AIR_THRESHOLD) && (grayscale_data[2] <= IN_AIR_THRESHOLD));
+}
+
+
+bool LineFollower::verify_stop_condition()
+{
+    if (stop_counter >= STOP_VALUE)
+    {
+        robotVisionVoyager->stop();
+        return true;
+    }    
+    else
+    {
+        return false;
+    }
+}
+
+
+void LineFollower::search_line()
+{
+    if (verify_is_in_air())
+    {
+        robotVisionVoyager->stop();
+    }
+
+    if (last_state == State::LEFT)
+    {   
+        robotVisionVoyager->turn_right_max();
+        /* search right */
+        robotVisionVoyager->move_forward();
+    }
+    else if (last_state == State::RIGHT)
+    {
+        robotVisionVoyager->turn_left_max();
+        /* search left */
+        robotVisionVoyager->move_forward();
+    }
+        
+}
+
+
+State LineFollower::compute_next_state(vector<int> grayscale_line_status)
+{
+    if ((grayscale_line_status[0] == 0) && (grayscale_line_status[1] == 0) && (grayscale_line_status[2] == 0))
+    {
+        /* grayscale sensor does not see the line at all */
+        stop_counter += 1;
+        return State::UNKNOWN;
+    }
+    else if ((grayscale_line_status[0] == 0) && (grayscale_line_status[1] == 1) && (grayscale_line_status[2] == 1))
+    {
+        /* grayscale sensor sees the line with center and right sensor */
+        last_state = State::LEFT;
+        stop_counter = 0;
+        return State::LEFT;
+    }
+    else if ((grayscale_line_status[0] == 1) && (grayscale_line_status[1] == 1) && (grayscale_line_status[2] == 0))
+    {
+        /* grayscale sensor sees the line with center and left sensor */
+        last_state = State::RIGHT;
+        stop_counter = 0;
+        return State::RIGHT;
+    }
+    else if (grayscale_line_status[1] == 1)
+    {
+        /* grayscale sensor sees the line with center sensor */
+        stop_counter = 0;  ;
+        return State::FORWARD;
+    }
+    else if (grayscale_line_status[2] == 1)
+    {
+        /* grayscale sensor sees the line with right sensor */
+        last_state = State::LEFT;
+        stop_counter = 0;
+        return State::LEFT;
+    }
+    else if (grayscale_line_status[0] == 1)
+    {
+        /* grayscale sensor sees the line with left sensor */
+        last_state = State::RIGHT;
+        stop_counter = 0;
+        return State::RIGHT;
+    }
+}
+
+
+void LineFollower::execute_move(State current_state)
+{
+    if (current_state == State::LEFT)
+    {
+        robotVisionVoyager->turn_right_max();
+        robotVisionVoyager->move_forward();
+    }
+    else if (current_state == State::RIGHT)
+    {
+        robotVisionVoyager->turn_left_max();
+        robotVisionVoyager->move_forward();
+    }
+    else if (current_state == State::FORWARD)
+    {
+        robotVisionVoyager->set_dir_angle(DEFAULT_WHEEL_ANGLE);
+        robotVisionVoyager->move_forward();
+    }
+    else
+    {
+        search_line();
     }
 }
--- LineFollower/lineFollower.h
+++ LineFollower/lineFollower.h
@@ -5,20 +5,36 @@
 #include <vector>
 
 
+#define LINE_THRESHOLD 80
+#define STOP_VALUE 350
+#define IN_AIR_THRESHOLD 15
+
+
 using namespace std;
+
+
+enum class State {
+    UNKNOWN,
+    FORWARD,
+    LEFT,
+    RIGHT
+};
 
 
 class LineFollower
 {   
     static VisionVoyager* robotVisionVoyager;
     static vector<int> grayscale_data;
+    static int stop_counter;
+    static State last_state;
     static void get_grayscale_data();
     static vector<int> get_line_from_grayscale_data();
-    // static void search_line();
-    // static void compute_next_state();
-    // static void execute_move();
-    // static bool verify_stop_condition(int stop_value);
-    // static bool verify_is_on_air();
+    static bool verify_stop_condition();
+    static bool verify_is_in_air();
+    static void search_line();
+    static State compute_next_state(vector<int> grayscale_line_status);
+    static void execute_move(State current_state);
+
 
 public:
     static void setRobot(VisionVoyager* robot);
