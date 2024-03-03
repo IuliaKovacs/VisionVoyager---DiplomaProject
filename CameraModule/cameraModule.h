1386ffd304a6cfb53f94c4e2831db61b543136eb
IuliaKovacs <101547437+IuliaKovacs@users.noreply.github.com>
2024-03-03 23:29:12 +0200

    Added FaceRecognition feature (basic implementation, will be developed further)

--- CameraModule/cameraModule.h
+++ CameraModule/cameraModule.h
@@ -2,13 +2,15 @@
 #define CAMERA_MODULE
 
 
-
+#include <opencv2/core.hpp>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <filesystem>
+#include <vector>
 
 namespace fs = std::filesystem;
+using namespace std;
 
 #define RAW_IMAGES_FOLDER_PATH "../CameraModule/Raw_Images"
 #define FACES_DATASET_PATH "../CameraModule/Faces_dataset"
@@ -18,16 +20,21 @@ namespace fs = std::filesystem;
 
 class CameraModule
 {   
+    static vector<cv::Mat> images;
+    static vector<int> labels;  
     static int no_of_recognized_subjects;
     static bool resize_and_apply_grayscale(const fs::path& input_path, int subject_number);
     static int count_recognized_subjects();
     static bool parse_raw_images_folder();
+    static void read_images_from_csv();
+    static bool check_data_valid();
 
 public:
     static void initialize_camera_module();
     static void display_camera_capture();
     static void add_new_recognized_subject();
     static void create_csv_database_file();
+    static int recognize_face(string test_image_path);
 
 };
 
---
+++ CameraModule/faceRecognition.cpp
@@ -0,0 +1,81 @@
+
+
+#include "cameraModule.h"
+#include <opencv2/core.hpp>
+#include <opencv2/face.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+
+
+using namespace std;
+
+
+vector<cv::Mat> CameraModule::images;
+vector<int> CameraModule::labels;  
+
+
+void CameraModule::read_images_from_csv()
+{   
+    string line;
+    ifstream CSV_file(CSV_FILE_PATH);
+
+    if (CSV_file.is_open())
+    {
+        while (getline(CSV_file,line))
+        {
+            while (!line.empty() && isspace(line.back())) 
+            {
+                line.pop_back();
+            }
+
+            string image_path = line.substr(0, line.find(';'));
+            string label = line.substr(line.find(';') + 1);
+
+            images.push_back(cv::imread(image_path, 0));
+            labels.push_back(atoi(label.c_str()));
+        }
+    }
+    else
+    {
+        cout << "Error: Unable to open CSV file" << endl;
+    }
+}
+
+
+bool CameraModule::check_data_valid()
+{
+    if(1 >= images.size()) 
+    {
+        cout << "The face recognition needs at least 2 images in order to work" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+
+int CameraModule::recognize_face(string test_image_path)
+{
+    read_images_from_csv();
+    check_data_valid();
+
+    cv::Mat test_image = cv::imread(test_image_path, 0);
+
+    if (test_image.empty()) 
+    {
+        cout << "Failed to load test image." << endl;
+    }
+
+    /* Create an Eigenfaces model and train it with the Faces_dataset retrieved data */
+    cv::Ptr<cv::face::EigenFaceRecognizer> model = cv::face::EigenFaceRecognizer::create();
+    model->train(images, labels);
+
+    int predicted_label = -1;
+    double confidence = 0.0;
+    model->predict(test_image, predicted_label, confidence);
+
+    cout << "Predicted class = " << predicted_label << ", Confidence = " << confidence << endl;
+
+    return predicted_label;
+}
